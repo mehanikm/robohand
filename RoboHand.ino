59e885567cd2d4052d97738dfce82b670ac8c718
Михаил Марченко <mehanikm@MacBook-ProM.local>
2020-06-03 20:58:54 +0200

    Add middle joint constrain

--- RoboHand.ino
+++ RoboHand.ino
@@ -22,41 +22,41 @@
 //EASE_USER         //
 
 ServoEasing base_rotation, base_lift, middle_lift, grip;  //  Create 4 servos for each joint
-MPU6050 accelerometer;   // Create accelerometer and gyroscope sensor
+MPU6050 accelerometer;                                    //  Create accelerometer and gyroscope sensor
 
 
-float acceleration_x, acceleration_y, acceleration_z;   //  Variables to store x,y,z accelerations from sensor
-int brot_deg, blift_deg, mlift_deg; //  Variables to define position of servos
-bool is_grip;  //  Variable to activate grip
-bool connection;  //  Is accelerometer connected?
+long acceleration_x, acceleration_y, acceleration_z;      //  Variables to store x,y,z accelerations from sensor
+int potent;                                               //  Variable to store reading from potentiometer
+long brot_deg, blift_deg, mlift_deg;                      //  Variables to define position of servos
+float low_bound_lift = 50, high_bound_lift = 180;         //  Boundaries for middle joint(to not to collide with things or suffer back overweight)
+bool is_grip;                                             //  Variable to activate grip
+bool connection;                                          //  Is accelerometer connected?
 
-const int n = 20;   // Mean array size
-int y[n], x[n];     // Mean arrays
-int i = 0;          // Array index
+const int n = 10;               // Size of arrays of values from sensors
+int y[n], x[n], p[n];           // Arrays of values from sensors
+int i = 0;                      // Arrays index
 
-float max_from_acc = 16000;   // Maximum value we can get from accelerometer to constrain to it
-
-void round_to_ten(int *num);
+float max_from_acc = 16000;     // Maximum value we can get from accelerometer to constrain to it
 
 
 
 void setup() {
-  Serial.begin(9600);   //  Start serial monitor and plotter to examine necessary information
+  Serial.begin(9600);
   Serial.println("Starting sketch............");
 
-  accelerometer.initialize();   //  Start accelerometer
+  accelerometer.initialize();       //  Start accelerometer
 
   base_rotation.attach(BROT_SERVO); //  Start servos
   base_lift.attach(BLIFT_SERVO);    //  Start servos
   middle_lift.attach(MLIFT_SERVO);  //  Start servos
   grip.attach(GRIP_SERVO);          //  Start servos
 
-  base_rotation.write(0);                       ////
-  base_rotation.setEasingType(EASE_LINEAR);     //
-  base_lift.write(0);                           //  Setup initial position and apply smoothing to servos' movement
-  base_lift.setEasingType(EASE_LINEAR);         //
-  middle_lift.write(0);                         //
-  middle_lift.setEasingType(EASE_LINEAR);       ////
+  base_rotation.write(0);                         ////
+  base_rotation.setEasingType(EASE_LINEAR);       //
+  base_lift.write(20);                            //  Setup initial position and apply smoothing to servos' movement
+  base_lift.setEasingType(EASE_LINEAR);           //
+  middle_lift.write(0);                           //
+  middle_lift.setEasingType(EASE_LINEAR);         ////
 
   Serial.println("Testing connection............");
   do {
@@ -65,9 +65,11 @@ void setup() {
   } while (!connection);
   Serial.println("Conncetion successful");
 
+
   for (int j = 0; j < n; j++) {                   ////
     y[j] = accelerometer.getAccelerationY();      //  Fill arrays with initial values so they are not empty by the time servos start
     x[j] = accelerometer.getAccelerationX();      //
+    p[j] = analogRead(0);                         //
   }                                               ////
 
 }
@@ -77,36 +79,44 @@ void setup() {
 void loop() {
   acceleration_x = accelerometer.getAccelerationX();  //Read X from accelerometer
   acceleration_y = accelerometer.getAccelerationY();  //Read Y from accelerometer
-  mlift_deg = analogRead(0);                          //Read value from petentiometer
+  potent = analogRead(0);                             //Read value from petentiometer
 
-  Serial.println("X: " + String(acceleration_x) + "\tY: " + String(acceleration_y));
-
-
-  brot_deg = constrain(mean(y, n), -max_from_acc, max_from_acc);   //  Constrain values from accelerometer and convert them to degrees
-  brot_deg = map(brot_deg, -max_from_acc, max_from_acc, 0, 180);        ////  -Base rotation
-
-  blift_deg = constrain(mean(x, n), -max_from_acc, max_from_acc);
-  blift_deg = map(blift_deg, -max_from_acc, max_from_acc, 0, 180);      ////  -Base lift
-
-  mlift_deg = map(mlift_deg, 0, 1023, 0, 180);                          ////  -Middle lift
+  //  Serial.println("X: " + String(acceleration_x) + "\tY: " + String(acceleration_y));
 
 
-  //  Serial.println("X: " + String(blift_deg) + "\tY: " + String(brot_deg));
+  brot_deg = constrain(mean(y, n), -max_from_acc, max_from_acc);            //  Constrain values from accelerometer and convert them to degrees
+  brot_deg = map(brot_deg, -max_from_acc, max_from_acc, 0, 180);            ////  -Base rotation
+
+  blift_deg = constrain(mean(x, n), -max_from_acc, max_from_acc);           //
+  blift_deg = map(blift_deg, -max_from_acc, max_from_acc, 0, 150);          ////  -Base lift
+
+
+  low_bound_lift = 60.0 - float(blift_deg) * .5;                            //  Calculate low degree bound for middle joint
+  high_bound_lift = 300.0 - float(blift_deg) * 2;                           //  Calculate high degree bound for middle joint
+  low_bound_lift = constrain(low_bound_lift, 0.0, 50.0);                    //  Constrain bounds
+  high_bound_lift = constrain(high_bound_lift, 0.0, 180.0);                 //  Constrain bounds
+  mlift_deg = map(mean(p, n), 0, 1023, low_bound_lift, high_bound_lift);    ////  -Middle lift
+
+
+  Serial.println("DegBR: " + String(brot_deg) + "\tDegBL: " + String(blift_deg) + \
+                 "\tDegML: " + String(mlift_deg) + "\tLow: " + String(low_bound_lift) +  \
+                 "\tHigh: " + String(high_bound_lift));
 
 
   base_rotation.startEaseTo(brot_deg, 270);           ////
   base_lift.startEaseTo(180 - blift_deg, 270);        //  Write degrees to servos
-  //  middle_lift.startEaseTo(mlift_deg, 270);        ////
+  middle_lift.startEaseTo(mlift_deg, 360);            ////
 
 
   y[i] = acceleration_y;      ////
   x[i] = acceleration_x;      //  Replace old values in array with actual
+  p[i] = potent;              //
   i++;                        //
   i %= n;                     ////
 
-  
+
   //  Serial.println(accelerometer.testConnection());
-  //  delay(10);
+  //    delay(100);
 }
 
 
