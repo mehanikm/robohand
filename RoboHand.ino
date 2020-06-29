93e4d09b1b4306974878f8dc9348aeee955ecab7
Mykhailo Marchenko <mehanikm@MacBook-ProM.local>
2020-06-29 23:50:04 +0200

    Add grip

--- RoboHand.ino
+++ RoboHand.ino
@@ -7,7 +7,9 @@
 #define BROT_SERVO  10  //  base_rotation servo
 #define BLIFT_SERVO 9   //  base_lift servo
 #define MLIFT_SERVO 6   //  middle_lift servo
-#define GRIP_SERVO  5   //  grip servo
+#define GRIP_SERVO  12   //  grip servo
+#define mid_pot     0  //  potentiometer for middle joint
+#define grip_pot    1  //  potentiometer for grip
 
 
 //EASE_LINEAR       //
@@ -27,9 +29,9 @@ MPU6050 accelerometer;                                    //  Create acceleromet
 
 long acceleration_x, acceleration_y, acceleration_z;      //  Variables to store x,y,z accelerations from sensor
 int potent;                                               //  Variable to store reading from potentiometer
-long brot_deg, blift_deg, mlift_deg;                      //  Variables to define position of servos
+long brot_deg, blift_deg, mlift_deg, grip_deg;            //  Variables to define position of servos
 float low_bound_lift = 50, high_bound_lift = 180;         //  Boundaries for middle joint(to not to collide with things or suffer back overweight)
-bool is_grip;                                             //  Variable to activate grip
+float grip_low_bound = 130, grip_high_bound = 180;        //  Boundaries for grip servo, for it not to break joints
 bool connection;                                          //  Is accelerometer connected?
 
 const int n = 10;               // Size of arrays of values from sensors
@@ -53,10 +55,12 @@ void setup() {
 
   base_rotation.write(0);                         ////
   base_rotation.setEasingType(EASE_LINEAR);       //
-  base_lift.write(20);                            //  Setup initial position and apply smoothing to servos' movement
+  base_lift.write(20);                            //
   base_lift.setEasingType(EASE_LINEAR);           //
-  middle_lift.write(0);                           //
-  middle_lift.setEasingType(EASE_LINEAR);         ////
+  middle_lift.write(0);                           //  Setup initial position and apply smoothing to servos' movement
+  middle_lift.setEasingType(EASE_LINEAR);         //
+  grip.write(grip_low_bound);                       //
+  grip.setEasingType(EASE_LINEAR);                ////
 
   Serial.println("Testing connection............");
   do {
@@ -69,7 +73,7 @@ void setup() {
   for (int j = 0; j < n; j++) {                   ////
     y[j] = accelerometer.getAccelerationY();      //  Fill arrays with initial values so they are not empty by the time servos start
     x[j] = accelerometer.getAccelerationX();      //
-    p[j] = analogRead(0);                         //
+    p[j] = analogRead(mid_pot);                   //
   }                                               ////
 
 }
@@ -79,7 +83,8 @@ void setup() {
 void loop() {
   acceleration_x = accelerometer.getAccelerationX();  //Read X from accelerometer
   acceleration_y = accelerometer.getAccelerationY();  //Read Y from accelerometer
-  potent = analogRead(0);                             //Read value from petentiometer
+  potent = analogRead(mid_pot);                       //Read value from petentiometer
+  grip_deg = analogRead(grip_pot);
 
   //  Serial.println("X: " + String(acceleration_x) + "\tY: " + String(acceleration_y));
 
@@ -96,6 +101,7 @@ void loop() {
   low_bound_lift = constrain(low_bound_lift, 0.0, 50.0);                    //  Constrain bounds
   high_bound_lift = constrain(high_bound_lift, 0.0, 180.0);                 //  Constrain bounds
   mlift_deg = map(mean(p, n), 0, 1023, low_bound_lift, high_bound_lift);    ////  -Middle lift
+  grip_deg = map(grip_deg, 0, 1023, grip_low_bound, grip_high_bound);
 
 
   Serial.println("DegBR: " + String(brot_deg) + "\tDegBL: " + String(blift_deg) + \
@@ -106,6 +112,7 @@ void loop() {
   base_rotation.startEaseTo(brot_deg, 270);           ////
   base_lift.startEaseTo(180 - blift_deg, 270);        //  Write degrees to servos
   middle_lift.startEaseTo(mlift_deg, 360);            ////
+  grip.startEaseTo(grip_deg, 360);
 
 
   y[i] = acceleration_y;      ////
